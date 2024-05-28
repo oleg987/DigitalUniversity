ed91d0e2c13a48f873578ea93da24502285e1a1c
Oleg <fruntovoleg@gmail.com>
2024-05-28 11:42:27 +0300

    add IEventConsumer interface.

---
+++ Common/Consumers/IEventConsumer.cs
@@ -0,0 +1,9 @@
+﻿using Common.Events.Abstractions;
+
+namespace Common.Consumers;
+
+public interface IEventConsumer<in TEvent>
+    where TEvent : EventBase
+{
+    Task Consume(CancellationToken cancellationToken = default);
+}
