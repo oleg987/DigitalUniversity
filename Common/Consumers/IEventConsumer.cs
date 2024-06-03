624ab6db64710fe522d2d817447b0248d2b59f2c
last_kaiten <fruntovoleg@gmail.com>
2024-06-03 09:18:49 +0300

    Remove IEventConsumer interface.

--- Common/Consumers/IEventConsumer.cs
+++
@@ -1,9 +0,0 @@
-﻿using Common.Events.Abstractions;
-
-namespace Common.Consumers;
-
-public interface IEventConsumer<in TEvent>
-    where TEvent : EventBase
-{
-    Task Consume(CancellationToken cancellationToken = default);
-}
