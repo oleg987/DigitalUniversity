cb1a2e0e325e458902a532265705d270b73b692e
last_kaiten <fruntovoleg@gmail.com>
2024-06-10 12:03:36 +0300

    Add enpoints for getting Subject List, Student Subjects and Professor Subjects.

--- UserService/Controllers/SubjectController.cs
+++ UserService/Controllers/SubjectController.cs
@@ -1,8 +1,10 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using UserService.Commands;
 using UserService.Data;
 using UserService.Requests;
+using UserService.Responses;
 
 namespace UserService.Controllers;
 
@@ -34,5 +36,40 @@ public class SubjectController : ControllerBase
         {
             return BadRequest();
         }
+    }
+
+    [HttpGet("{studentId:guid}")]
+    public async Task<IActionResult> ByStudentId([Required] Guid studentId, CancellationToken cancellationToken)
+    {
+        var subjects = await _context.Subjects
+            .Where(s => s.Students.Any(st => st.Id == studentId))
+            .OrderBy(s => s.Title)
+            .Select(s => new SubjectResponse(s.Id, s.Title))
+            .ToListAsync(cancellationToken);
+
+        return Ok(subjects);
+    }
+    
+    [HttpGet("{professorId:guid}")]
+    public async Task<IActionResult> ByProfessorId([Required] Guid professorId, CancellationToken cancellationToken)
+    {
+        var subjects = await _context.Subjects
+            .Where(s => s.Professor.Id == professorId)
+            .OrderBy(s => s.Title)
+            .Select(s => new SubjectResponse(s.Id, s.Title))
+            .ToListAsync(cancellationToken);
+
+        return Ok(subjects);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> List(CancellationToken cancellationToken)
+    {
+        var subjects = await _context.Subjects
+            .OrderBy(s => s.Title)
+            .Select(s => new SubjectResponse(s.Id, s.Title))
+            .ToListAsync(cancellationToken);
+
+        return Ok(subjects);
     }
 }
