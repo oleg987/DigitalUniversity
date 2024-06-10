e3468d22e2e7102d19f6b8595a03b4a9148d1850
last_kaiten <fruntovoleg@gmail.com>
2024-06-10 15:00:18 +0300

    Refactor CreateSubjectCommand

--- UserService/Commands/CreateSubjectCommand.cs
+++ UserService/Commands/CreateSubjectCommand.cs
@@ -10,16 +10,18 @@ public class CreateSubjectCommand : ICommand
 {
     private readonly UserDbContext _ctx;
     private readonly CreateSubjectRequest _request;
+    private readonly Guid _professorId;
 
-    public CreateSubjectCommand(UserDbContext ctx, CreateSubjectRequest request)
+    public CreateSubjectCommand(UserDbContext ctx, CreateSubjectRequest request, Guid professorId)
     {
         _ctx = ctx;
         _request = request;
+        _professorId = professorId;
     }
 
     public async Task Execute(CancellationToken cancellationToken = default)
     {
-        var professor = await _ctx.Professors.SingleAsync(p => p.Id == _request.ProfessorId, cancellationToken);
+        var professor = await _ctx.Professors.SingleAsync(p => p.Id == _professorId, cancellationToken);
 
         var subject = new Subject(_request.Id, _request.Title, professor);
 
--- UserService/Requests/CreateSubjectRequest.cs
+++ UserService/Requests/CreateSubjectRequest.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace UserService.Requests;
 
-public record CreateSubjectRequest([Required] Guid ProfessorId, [Required] string Title)
+public record CreateSubjectRequest([Required] string Title)
 {
     public Guid Id { get; } = Guid.NewGuid();
 }
