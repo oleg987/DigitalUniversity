19188c7de109e6186dedd8278a031fa45b7a00aa
last_kaiten <fruntovoleg@gmail.com>
2024-06-10 13:52:48 +0300

    Create enpoints Login, Refresh, Activate in AuthController.

--- AuthService/Controllers/AuthController.cs
+++ AuthService/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 using System.ComponentModel.DataAnnotations;
+using AuthService.Commands;
+using AuthService.Data;
 using AuthService.Requests;
+using AuthService.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthService.Controllers;
@@ -8,21 +11,59 @@ namespace AuthService.Controllers;
 [Route("api/[controller]/[action]")]
 public class AuthController : ControllerBase
 {
+    private readonly AuthDbContext _context;
+    private readonly JwtAuthService _jwtAuthService;
+
+    public AuthController(AuthDbContext context, JwtAuthService jwtAuthService)
+    {
+        _context = context;
+        _jwtAuthService = jwtAuthService;
+    }
+
     [HttpPost]
     public async Task<IActionResult> Login([Required] LoginRequest request, CancellationToken cancellationToken)
     {
-        return Ok();
+        try
+        {
+            var token = await _jwtAuthService.LoginAsync(request, cancellationToken);
+
+            return Ok(token);
+        }
+        catch
+        {
+            return Unauthorized();
+        }
     }
     
     [HttpPost]
     public async Task<IActionResult> Refresh([Required] RefreshTokenRequest request, CancellationToken cancellationToken)
     {
-        return Ok();
+        try
+        {
+            var token = await _jwtAuthService.RefreshAsync(request, cancellationToken);
+
+            return Ok(token);
+        }
+        catch
+        {
+            return Unauthorized();
+        }
     }
     
     [HttpPost]
     public async Task<IActionResult> Activate([Required] ActivateUserRequest request, CancellationToken cancellationToken)
     {
-        return Ok();
+        try
+        {
+            var command = new ActivateUserCommand(_context, request);
+
+            await command.Execute(cancellationToken);
+
+            return NoContent();
+        }
+        catch
+        {
+            return BadRequest();
+        }
     }
 }
