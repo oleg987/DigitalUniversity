01955171b3ff1eaf8d7e382add3b841083797004
last_kaiten <fruntovoleg@gmail.com>
2024-06-10 15:00:34 +0300

    Refactor SelectSubjectCommand

--- UserService/Commands/SelectSubjectCommand.cs
+++ UserService/Commands/SelectSubjectCommand.cs
@@ -9,16 +9,18 @@ public class SelectSubjectCommand : ICommand
 {
     private readonly UserDbContext _context;
     private readonly SelectSubjectRequest _request;
+    private readonly Guid _studentId;
 
-    public SelectSubjectCommand(UserDbContext context, SelectSubjectRequest request)
+    public SelectSubjectCommand(UserDbContext context, SelectSubjectRequest request, Guid studentId)
     {
         _context = context;
         _request = request;
+        _studentId = studentId;
     }
 
     public async Task Execute(CancellationToken cancellationToken = default)
     {
-        var student = await _context.Students.SingleAsync(s => s.Id == _request.StudentId, cancellationToken);
+        var student = await _context.Students.SingleAsync(s => s.Id == _studentId, cancellationToken);
 
         var subject = await _context.Subjects.SingleAsync(s => s.Id == _request.SubjectId, cancellationToken);
         
--- UserService/Requests/SelectSubjectRequest.cs
+++ UserService/Requests/SelectSubjectRequest.cs
@@ -2,4 +2,4 @@ using System.ComponentModel.DataAnnotations;
 
 namespace UserService.Requests;
 
-public record SelectSubjectRequest([Required]Guid StudentId, [Required]Guid SubjectId);
+public record SelectSubjectRequest([Required]Guid SubjectId);
